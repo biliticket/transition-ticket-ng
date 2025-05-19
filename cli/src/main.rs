ed019eb80166dd5d9bbef2a4461a5ef9d6ef22a3
Hobr <mail@hobr.site>
2025-05-19 15:07:06 +0800

    app: 无用导入

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,3 @@
-use task;
-use util;
-
 fn main() {
     println!("Hello, world! {} {}", util::add(2, 2), task::add(2, 2));
 }
--- task/src/lib.rs
+++ task/src/lib.rs
@@ -1,5 +1,3 @@
-use util;
-
 pub fn add(left: u64, right: u64) -> u64 {
     util::add(left, right)
 }
